80801ec4ae7d51a924809478a976bad5d8560a41
Dany Bechara <dbechara@MCLCRXVFL>
2022-08-01 15:05:16 -0400

    f

---
+++ .history/models/UsuarioEimagen_20220801145911.ts
@@ -0,0 +1,8 @@
+import { Schema,model } from "mongoose";
+
+const UsuarioImagenSchema = new Schema({
+    usuario:{
+        type:Schema.Types.ObjectId,
+        ref:
+    }
+})
--- dist/models/UsuarioEimagen.js
+++ dist/models/UsuarioEimagen.js
@@ -1,1 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const UsuarioImagenSchema = new mongoose_1.Schema({
+    usuario: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: 'Usuario'
+    },
+    img: {
+        type: String,
+        required: [true, 'IMG is necesary']
+    }
+});
+exports.default = (0, mongoose_1.model)('UsuarioImagen', UsuarioImagenSchema);
 //# sourceMappingURL=UsuarioEimagen.js.map
