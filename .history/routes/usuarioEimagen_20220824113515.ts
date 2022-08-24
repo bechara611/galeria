60d9db10bf68b9c4ebe2b42e9f7a88a4202d2e90
Dany Bechara <dbechara@MCLCRXVFL>
2022-08-24 15:52:09 -0400

    f

---
+++ .history/routes/usuarioEimagen_20220824113515.ts
@@ -0,0 +1,19 @@
+import {Router} from 'express';
+import { deleteUsuarioImagen, getUsuarioEimagenes, getUsuarioEimagenesPorId } from '../controllers/usuarioEimagen';
+import {check} from 'express-validator'
+import { comprobarCampos } from '../helpers/expressValidator';
+const router= Router();
+
+
+//obtener todas las imagenes o link de imagenes con respecto a un ID de un usuario
+router.post('/',[
+    check('idUsuario2','Error, USER MongoID not valid').isMongoId(),
+    comprobarCampos
+],getUsuarioEimagenesPorId)
+
+router.delete('/',[
+    check('id_imagenes','Error, DATA NOT FOUND ( id_imagenes )').not().isEmpty(),
+    comprobarCampos
+],deleteUsuarioImagen)
+
+export default router;
